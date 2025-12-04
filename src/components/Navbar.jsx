99753b58156cd02b4bd5e9dcf5a1eb791d7c4022
Reddisekharyadav <reddisekharmarugani@gmail.com>
2025-12-04 12:06:46 +0530

    Update logo path and add glowing effects to navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             <div className="absolute inset-0 bg-[#00d9ff] blur-xl opacity-0 group-hover:opacity-50 transition-opacity duration-300 rounded-full"></div>
             <img 
               src="/Logo1.png" 
-              alt="logo" 
+              alt="logo1" 
               className="w-9 h-9 object-contain relative z-10 drop-shadow-[0_0_8px_rgba(0,217,255,0.6)] group-hover:drop-shadow-[0_0_15px_rgba(57,255,20,0.8)] transition-all duration-300 group-hover:scale-110" 
             />
           </div>
